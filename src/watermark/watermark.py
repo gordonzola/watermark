5f55e2d2668daadeba0782032ec760331aa3b989
Damien Nicolas <damien@gordon.re>
2016-06-27 16:07:36 +0200

    Add opacity argument

--- src/watermark/watermark.py
+++ src/watermark/watermark.py
@@ -10,8 +10,17 @@
 Main Watermark script
 """
 import argparse
+from functools import partial
 
 from PIL import Image
+
+
+def arg_type(x, min_val, max_val):
+    x = int(x)
+    if x < min_val or x > max_val:
+        raise argparse.ArgumentTypeError('Value must be between {} and {}'
+                                         .format(min_val, max_val))
+    return x
 
 
 def main():
@@ -19,9 +28,16 @@ def main():
     parser.add_argument('source', type=argparse.FileType(mode='rb'),
                         help='Source image')
     parser.add_argument('-o', '--output', type=argparse.FileType(mode='wb'),
+                        required=True,
                         help='Destination for watermarked image')
     parser.add_argument('-w', '--watermark', type=argparse.FileType(mode='rb'),
+                        required=True,
                         help='Watermark image')
+    parser.add_argument('--opacity', type=partial(arg_type, min_val=0,
+                                                  max_val=100),
+                        default=50,
+                        help='Watermark opacity (from 0, transparent, to 100, '
+                             'opaque)')
 
     args = parser.parse_args()
 
@@ -32,7 +48,7 @@ def main():
     watermark = Image.open(args.watermark)
     if watermark.mode != 'RGBA':
         watermark = watermark.convert('RGBA')
-    mask = watermark.split()[3].point(lambda i: i * .3)
+    mask = watermark.split()[3].point(lambda i: i * args.opacity / 100)
     w_xsize, w_ysize = watermark.size
     for pos_x in range(0, s_xsize, w_xsize):
         for pos_y in range(0, s_ysize, w_ysize):
