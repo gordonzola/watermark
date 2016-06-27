8d9ac634c91a533534d44fa65ba521145f201993
Damien Nicolas <damien@gordon.re>
2016-06-27 16:39:31 +0200

    Add thumbnail option

--- src/watermark/watermark.py
+++ src/watermark/watermark.py
@@ -10,6 +10,7 @@
 Main Watermark script
 """
 import argparse
+import re
 from functools import partial
 
 from PIL import Image
@@ -21,6 +22,13 @@ def arg_type(x, min_val, max_val):
         raise argparse.ArgumentTypeError('Value must be between {} and {}'
                                          .format(min_val, max_val))
     return x
+
+
+def dimensions(s):
+    match = re.match(r'^(\d+)x(\d+)$', s)
+    if match is None:
+        raise argparse.ArgumentTypeError('Incorrect dimensions')
+    return [int(i) for i in match.groups()]
 
 
 def main():
@@ -38,12 +46,32 @@ def main():
                         default=50,
                         help='Watermark opacity (from 0, transparent, to 100, '
                              'opaque)')
+    parser.add_argument('--thumbnail', type=dimensions,
+                        help='Generates a thumbnail (ex: 100x150)')
 
     args = parser.parse_args()
 
     source = Image.open(args.source)
     if source.mode != 'RGBA':
         source = source.convert('RGBA')
+    if args.thumbnail:
+        thumb_ratio = args.thumbnail[0] / args.thumbnail[1]
+        source_ratio = source.size[0] / source.size[1]
+        if thumb_ratio != source_ratio:
+            if source_ratio > thumb_ratio:
+                thumb_y = source.size[1]
+                thumb_x = thumb_y * thumb_ratio
+                thumb_size = (thumb_x, thumb_y)
+            elif source_ratio < thumb_ratio:
+                thumb_x = source.size[0]
+                thumb_y = thumb_x / thumb_ratio
+                thumb_size = (thumb_x, thumb_y)
+            box = (source.size[0] / 2 - thumb_size[0] / 2,
+                   source.size[1] / 2 - thumb_size[1] / 2,
+                   source.size[0] / 2 + thumb_size[0] / 2,
+                   source.size[1] / 2 + thumb_size[1] / 2)
+            source = source.crop(box)
+        source.thumbnail(args.thumbnail)
     s_xsize, s_ysize = source.size
     watermark = Image.open(args.watermark)
     if watermark.mode != 'RGBA':
